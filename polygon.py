0138810e7cc22340638ff9aea297f5df8e1b4014
aakash <aakashjaiswal90261@gmail.com>
2023-08-11 20:05:28 +0530

    game

---
+++ polygon.py
@@ -0,0 +1,40 @@
+from turtle import *
+
+def triangle():
+    for i in range(3):
+        fd(100)
+        lt(360/3)
+
+def square():
+    for i in range(4):
+        fd(100)
+        lt(360/4)
+
+def pentgon():
+    for i in range(5):
+        fd(100)
+        lt(360/5)        
+def hexagon():
+    for i in range(6):
+        fd(100)
+        lt(360/6)
+def heptagon():
+    for i in range(7):
+        fd(100)
+        lt(360/7)
+def octagon():
+    for i in range(8):
+        fd(100)
+        lt(360/8)   
+def nonegon():
+    for i in range(9):
+        fd(100)
+        lt(360/9)   
+def decagon():
+    for i in range(10):
+        fd(100)
+        lt(360/10)  
+
+print(__name__)
+if __name__== "__main__":
+    print('this is running as current file')
